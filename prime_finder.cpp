24d5acdbcade7b2e8c65d2c96cc5e7d71a4c85aa
Anadon <joshua.r.marshall.1991@gmail.com>
2021-04-01 17:05:28 -0400

    Added a sieve algorithm contributed by a friend, it is 50X faster than what I had.

--- prime_finder.cpp
+++ prime_finder.cpp
@@ -10,6 +10,9 @@ the square-root of that number and if a prime is found to be a
 factor then don't add it to the list.  If no factor is found
 to evenly divide a number, then it is appended to the list of
 found primes.
+
+NOTE: Added a comparison algorithm a friend contributed, and it makes a very
+strong argument that the sieve approach is faster.
 */
 
 #include <array>
@@ -17,57 +20,98 @@ found primes.
 #include <iostream>
 #include <chrono>
 #include <utility>
+#include <vector>
+#include <string>
 
 
 template<
   int upper_bound>
-std::pair<
-  std::array<int, 1+(upper_bound/2)>,
-  int>
-find_primes_less_than(){
-  std::array<int, 1+(upper_bound/2)> found_primes;
-  size_t end_index = 0;
-  found_primes[end_index++] = 2;
+std::vector<bool>
+prime_sieve_less_than(){
+  std::vector<bool> prime_listing(1+(upper_bound/2), true); // All odd primes, by 2n+1 index.
 
-  for(int number = 3; number <= upper_bound; number += 2){
+  constexpr int sieve_input_upper_bound = ((int)sqrt(upper_bound))/2;
+
+  prime_listing[0] = false; // 1 is not prime.
+  for(size_t prime_idx = 1; prime_idx < sieve_input_upper_bound; prime_idx++) {
+    if(!prime_listing[prime_idx]) {
+      continue;
+    }
+    size_t iter = prime_idx * 2 + 1;
+    for(size_t i = prime_idx + iter; i < (1+(upper_bound/2)); i += iter) {
+      prime_listing[i] = false;
+    }
+  }
+
+  return prime_listing;
+}
+
+
+template<
+  int upper_bound>
+std::vector<int>
+find_primes_less_than(){
+  std::vector<int> found_primes;
+  found_primes.reserve(1+(upper_bound/log(upper_bound)));
+  found_primes.push_back(2);
+  found_primes.push_back(3);
+
+  for(int number = 6; number <= upper_bound; number += 6){
+    const int num_m_one = number-1;
     bool factor_found = false;
-    for(size_t possible_factor_idx = 0; found_primes[possible_factor_idx] <= floor(sqrt(number)); ++possible_factor_idx){
-      if(! (number % found_primes[possible_factor_idx])){
+    for(auto itr = found_primes.begin(); *itr <= (int)sqrt(num_m_one); ++itr){
+      if(! (num_m_one % *itr)){
         factor_found = true;
         break;
       }
     }
     if(!factor_found)
-      found_primes[end_index++] = number;
+      found_primes.push_back(num_m_one);
+
+    const int num_p_one = number+1;
+    factor_found = false;
+    for(auto itr = found_primes.begin(); *itr <= (int)sqrt(num_p_one); ++itr){
+      if(! (num_p_one % *itr)){
+        factor_found = true;
+        break;
+      }
+    }
+    if(!factor_found)
+      found_primes.push_back(num_p_one);
   }
 
-  return {found_primes, end_index};
+  return found_primes;
 }
 
 
-int main(){
-  using namespace std::chrono_literals;
+template<class return_type>
+void run_primes_time_experiment(const char* desc, return_type (*prime_func) () ){
+  using namespace std::chrono;
 
-  constexpr int upper_bound = 1000;
-
-  std::pair<
-    std::array<int, 1+(upper_bound/2)>,
-    int> found_pair;
+  return_type found;
   int iterations = 0;
   std::chrono::time_point<std::chrono::system_clock> time_end;
   auto time_start = std::chrono::system_clock::now();
   do{
-    found_pair = find_primes_less_than<upper_bound>();
+    found = prime_func();
     iterations++;
   }while((time_end = std::chrono::system_clock::now()) - time_start < 5s );
 
   double time_duration = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::duration<double>(time_end-time_start)).count();
 
-  std::cout << time_duration / iterations << " seconds per prime-finding, with " << iterations << " iterations completed." << std::endl;
-  for(int i = 0; i < found_pair.second; ++i){
-    std::cout << found_pair.first[i] << ", ";
-  }
-  std::cout << std::endl;
+  std::cout << "With approach \"" << desc << "\": " << time_duration / iterations << " seconds per prime-finding, with " << iterations << " iterations completed." << std::endl;
+  // for(int i : found){
+  //   std::cout << i << ", ";
+  // }
+  // std::cout << std::endl;
+}
+
+
+int main(){
+  constexpr int upper_bound = 1000000;
+
+  run_primes_time_experiment<std::vector<int>>("list", find_primes_less_than<upper_bound>);
+  run_primes_time_experiment<std::vector<bool>>("sieve", prime_sieve_less_than<upper_bound>);
 
   return 0;
 }
